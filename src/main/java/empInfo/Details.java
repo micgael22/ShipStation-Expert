5427c0d88c631f93a4f28ff1d4e94625395763f8
MigoCode <micagel22gmail.com>
2022-09-27 12:29:28 +0200

    employee Information

---
+++ src/main/java/empInfo/Details.java
@@ -0,0 +1,32 @@
+package empInfo;
+
+public class Details {
+
+    public static void main(String[] args) {
+
+        EmpDetails emily = new EmpDetails("mngr423957","masujev");
+        EmpDetails jason = new EmpDetails("mngr423958","AtuzemY");
+        EmpDetails sheldon = new EmpDetails("mngr423959","ymyhyru");
+        EmpDetails bernadette = new EmpDetails("mngr423960","AtapEzA");
+        EmpDetails kaleb = new EmpDetails("mngr423961","hegYzAr");
+        EmpDetails ayabonga = new EmpDetails("mngr423962","numUpeh");
+        EmpDetails jenny = new EmpDetails("mngr423963","hArApAq");
+        EmpDetails riaad = new EmpDetails("mngr423964","gyqEdAb");
+        EmpDetails mac = new EmpDetails("mngr423965","esehesU");
+        EmpDetails malcom = new EmpDetails("mngr423967","ebUbupU");
+        EmpDetails shannon = new EmpDetails("mngr423968","utytuga");
+        EmpDetails gabrielle = new EmpDetails("mngr423969","dupYtUd");
+        EmpDetails mindy = new EmpDetails("mngr423970","UbazuqY");
+        EmpDetails nkosi = new EmpDetails("mngr423971","Amepapu");
+        EmpDetails precious = new EmpDetails("mngr423972","ApamAjE");
+        EmpDetails amy = new EmpDetails("mngr423973","tegajAg");
+        EmpDetails louis = new EmpDetails("mngr423974","hequmEz");
+        EmpDetails mandel = new EmpDetails("mngr423975","pErUrUz");
+        EmpDetails howard = new EmpDetails("mngr423976","qAmUjYv");
+        EmpDetails miles = new EmpDetails("mngr423977","enusytU");
+        EmpDetails fatima = new EmpDetails("mngr423978","UbemutU");
+
+        //System.out.println(emily.password);
+        //System.out.println(emily.userid);
+    }
+}
---
+++ src/main/java/empInfo/EmpDetails.java
@@ -0,0 +1,27 @@
+package empInfo;
+
+public class EmpDetails {
+    public String userid;
+    public String password;
+
+    public EmpDetails(String userid, String password) {
+        this.userid = userid;
+        this.password = password;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
