08f410713125688dd7da057ec32d80e7180bdd10
MigoCode <micagel22gmail.com>
2022-10-03 10:55:57 +0200

    allureReport screenshotFails

--- src/test/java/org/allureTestNG/AllureListenerNew.java
+++ src/test/java/org/allureTestNG/AllureListenerNew.java
@@ -1,6 +1,7 @@
 package org.allureTestNG;
 
 import io.qameta.allure.Allure;
+import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -12,17 +13,42 @@ import java.util.UUID;
 
 public class AllureListenerNew extends TestListenerAdapter {
 
-   @Override
-    public void onTestFailure(ITestResult result) {
-        System.out.println("Executing Listener on failure");
-        WebDriver driver = GoogleSearch.getDriverFromContext(result.getTestContext());
+//   @Override
+//    public void onTestFailure(ITestResult result) {
+//        System.out.println("Executing Listener on failure");
+//        WebDriver driver = GoogleSearch.getDriverFromContext(result.getTestContext());
+//
+//        try {
+//            Allure.addAttachment(UUID.randomUUID().toString(),
+//                    new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
-        try {
-            Allure.addAttachment(UUID.randomUUID().toString(),
-                    new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        private static String getTestMethodName(ITestResult result) {
+        return result.getMethod().getConstructorOrMethod().getName();
+    }
+        @Attachment(value= "Web Page screenshot", type = ".png")
+    public byte[] saveFailureScreenShot(WebDriver driver) { // saveFailureScreenShot
+        return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+    }
+
+        @Attachment(value = "{0}", type = "text")
+    public static String saveTextLog(String message) {
+        return message;
+    }
+
+           @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("I am in onTestFailure method " + getTestMethodName(result) + " failed");
+        Object testClass = result.getInstance();
+        WebDriver driver = GoogleSearch.getDriverFromContext(result.getTestContext());
+        // Allure ScreenShot and SaveTestLog
+        if (driver instanceof WebDriver) {
+            System.out.println("Screenshot captured for test case:" + getTestMethodName(result));
+            saveFailureScreenShot(driver);
         }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        saveTextLog(getTestMethodName(result) + " failed and screenshot taken!");
     }
 }
